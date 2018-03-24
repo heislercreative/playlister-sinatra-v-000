d4fb4a4684748e50cf8d242f17787c6c47889fa1
Brennan Heisler <learn@heislercreative.com>
2018-03-24 01:35:37 +0000

    automatically backed up by learn

--- app/models/Artist.rb
+++ app/models/Artist.rb
@@ -1,8 +1,8 @@
 require_relative './Slugify.rb'
 
 class Artist < ActiveRecord::Base
-  extend Slugify::find_by_slug
-  include Slugify::slug
+  extend Slugify::ClassMethod
+  include Slugify::InstanceMethod
 
   has_many :songs
   has_many :genres, through: :songs
